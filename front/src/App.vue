6ccd2c9acc97497d3deb8bca272de53a328edef5
nuyoi <nuyoi@naver.com>
2020-12-13 23:18:52 +0900

    로그인 & 회원 가입 페이지

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav-bar />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from "@/components/NavBar";
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style src="./assets/tailwind.css"/>
---
+++ front/src/views/signup/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <div
+    class="flex items-center justify-center min-h-screen px-4 py-12 bg-gray-50 sm:px-6 lg:px-8"
+  >
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img
+          class="w-auto h-12 mx-auto"
+          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+          alt="Workflow"
+        />
+        <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">
+          Sign up to your account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <div>
+          <label for="user-name" class="sr-only">User name</label>
+          <input
+            id="user-name"
+            name="name"
+            type="text"
+            autocomplete="name"
+            required
+            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="User name"
+            v-model="form.name"
+          />
+        </div>
+        <div>
+          <label for="email-address" class="sr-only">Email address</label>
+          <input
+            id="email-address"
+            name="email"
+            type="email"
+            autocomplete="email"
+            required
+            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Email address"
+            v-model="form.email"
+          />
+        </div>
+        <div>
+          <label for="password" class="sr-only">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            required
+            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Password"
+            v-model="form.password"
+          />
+        </div>
+        <div>
+          <label for="password-confirm" class="sr-only">Password confirm</label>
+          <input
+            id="password-confirm"
+            name="password-confirm"
+            type="password"
+            autocomplete="current-password"
+            required
+            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Password confirm"
+            v-model="form.passwordConfirm"
+          />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            @click.prevent="onSubmit"
+            class="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {},
+  },
+};
+</script>
